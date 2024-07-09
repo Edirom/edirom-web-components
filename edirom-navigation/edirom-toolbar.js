640089862014168932de8b093df4d866803d6901
Daniel Jettka <research@jettka.de>
2024-07-09 13:10:13 +0200

    added audio player component sample

--- edirom-navigation/edirom-toolbar.js
+++ edirom-navigation/edirom-toolbar.js
@@ -49,6 +49,10 @@ class EdiromToolbar extends HTMLElement{
             <ul>
             <li id="ne">New Edition</li> 
             </ul>
+            <p>Audio</p>
+            <ul>
+            <li id="ap">Audio Player</li> 
+            </ul>
             </div>
         </body>
         
@@ -79,6 +83,10 @@ class EdiromToolbar extends HTMLElement{
             });   
             this.shadowRoot.getElementById("ne").addEventListener('click', () => {
                 this.createWindow('[{"title": "New Edition", "html": "This is new edition"}]');
+            });   
+            this.shadowRoot.getElementById("ap").addEventListener('click', () => {
+                const audioPlayerElem = "<edirom-audio-player tracks='[ {&quot;title&quot;: &quot;Mercy&quot;, &quot;composer&quot;: &quot;Vernon Maytone&quot;, &quot;work&quot;: &quot;Brotheration Reggae 2015&quot;, &quot;src&quot;: &quot;https://pixabay.com/de/music/download/id-140277.mp3&quot;, &quot;type&quot;: &quot;audio/mpeg&quot;}, {&quot;title&quot;: &quot;Walk Good&quot;, &quot;composer&quot;: &quot;Daddy Roy&quot;, &quot;work&quot;: &quot;Brotheration Records 2016&quot;, &quot;src&quot;: &quot;https://pixabay.com/de/music/download/id-140285.mp3&quot;, &quot;type&quot;: &quot;audio/mpeg&quot;},  {&quot;title&quot;: &quot;To be free&quot;, &quot;composer&quot;: &quot;Composer 1&quot;, &quot;work&quot;: &quot;Own work&quot;, &quot;src&quot;: &quot;https://upload.wikimedia.org/wikipedia/commons/1/12/03may2020-tobefree.mp3&quot;, &quot;type&quot;: &quot;audio/mpeg&quot;}, {&quot;title&quot;: &quot;A Tale of Distant Lands&quot;, &quot;composer&quot;: &quot;Robert Schumann&quot;, &quot;work&quot;: &quot;Own work&quot;, &quot;src&quot;: &quot;https://upload.wikimedia.org/wikipedia/commons/e/ec/A_Tale_of_Distant_Lands.mp3&quot;, &quot;type&quot;: &quot;audio/mpeg&quot;},  {&quot;title&quot;: &quot;Funky Souls&quot;, &quot;composer&quot;: &quot;Amarià&quot;, &quot;work&quot;: &quot;Soundcloud: Funky Souls (2019)&quot;, &quot;src&quot;: &quot;https://upload.wikimedia.org/wikipedia/commons/2/2e/Amari%C3%A0_-_Funky_Souls_%282019%29.mp3&quot;, &quot;type&quot;: &quot;audio/mpeg&quot;}]' height='500px' width='500px' state='pause' track='0' start='0.0' end='' playbackrate='1.0' playbackmode='repeat' displaymode='controls-lg'></edirom-audio-player>";
+                this.createWindow('[{"title": "Edirom Audio Player", "html":"'+audioPlayerElem+'"}]');
             });   
         }
 
