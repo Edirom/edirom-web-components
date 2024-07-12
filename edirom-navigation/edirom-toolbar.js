36bbfb079e24e8b71abe4c90eb65d7e1cf20cdd4
Hizkiel <hizclick@gmail.com>
2024-07-12 14:59:07 +0200

    add attributeChangedCallback() on osd and verovio

--- edirom-navigation/edirom-toolbar.js
+++ edirom-navigation/edirom-toolbar.js
@@ -76,13 +76,16 @@ class EdiromToolbar extends HTMLElement{
                 this.createWindow('[{"title": "Critical Remarks", "html": "This is remarks"}]');
             });
             this.shadowRoot.getElementById("osd").addEventListener('click', () => {
-                this.createWindow('[{"title": "Open SeaDragon", "html": "<edirom-openseadragon></edirom-openseadragon>"}]');
+                const osdConfig = " <edirom-openseadragon  preserveViewport='true' visibilityRatio='1' minZoomLevel='1' maxZoomLevel='1' showNavigationControl='false' sequenceMode='true'></edirom-openseadragon>";
+                this.createWindow('[{"title": "Edirom Audio Player", "html":"'+osdConfig+'"}]');
             });
+
             this.shadowRoot.getElementById("fe").addEventListener('click', () => {
-                this.createWindow('[{"title": "Image from URL", "url": "https://klarinettenquintett.weber-gesamtausgabe.de/Scaler/IIIF/Erstdruck2!34_ED-st_II_002_Cl(B)_2r/full/666,/0/default.jpg"}]');
+            this.createWindow('[{"title": "Image from URL", "url": "https://klarinettenquintett.weber-gesamtausgabe.de/Scaler/IIIF/Erstdruck2!34_ED-st_II_002_Cl(B)_2r/full/666,/0/default.jpg"}]');
             });
             this.shadowRoot.getElementById("ve").addEventListener('click', () => {
-                this.createWindow('[{"title": "Verovio", "html": "<verovio-api-renderer></verovio-api-renderer>"}]');
+                const verovioConfig = "<verovio-api-renderer   width='200' zoom='20' height='300'  pagenumber='3'  meiurl='https://www.verovio.org/examples/downloads/Schubert_Lindenbaum.mei'></verovio-api-renderer>";
+                this.createWindow('[{"title": "Verovio", "html": "'+verovioConfig+'"}]');
             });
             this.shadowRoot.getElementById("ap").addEventListener('click', () => {
                 const audioPlayerElem = "<edirom-audio-player tracks='[ {&quot;title&quot;: &quot;Mercy&quot;, &quot;composer&quot;: &quot;Vernon Maytone&quot;, &quot;work&quot;: &quot;Brotheration Reggae 2015&quot;, &quot;src&quot;: &quot;https://pixabay.com/de/music/download/id-140277.mp3&quot;, &quot;type&quot;: &quot;audio/mpeg&quot;}, {&quot;title&quot;: &quot;Walk Good&quot;, &quot;composer&quot;: &quot;Daddy Roy&quot;, &quot;work&quot;: &quot;Brotheration Records 2016&quot;, &quot;src&quot;: &quot;https://pixabay.com/de/music/download/id-140285.mp3&quot;, &quot;type&quot;: &quot;audio/mpeg&quot;},  {&quot;title&quot;: &quot;To be free&quot;, &quot;composer&quot;: &quot;Composer 1&quot;, &quot;work&quot;: &quot;Own work&quot;, &quot;src&quot;: &quot;https://upload.wikimedia.org/wikipedia/commons/1/12/03may2020-tobefree.mp3&quot;, &quot;type&quot;: &quot;audio/mpeg&quot;}, {&quot;title&quot;: &quot;A Tale of Distant Lands&quot;, &quot;composer&quot;: &quot;Robert Schumann&quot;, &quot;work&quot;: &quot;Own work&quot;, &quot;src&quot;: &quot;https://upload.wikimedia.org/wikipedia/commons/e/ec/A_Tale_of_Distant_Lands.mp3&quot;, &quot;type&quot;: &quot;audio/mpeg&quot;},  {&quot;title&quot;: &quot;Funky Souls&quot;, &quot;composer&quot;: &quot;Amarià&quot;, &quot;work&quot;: &quot;Soundcloud: Funky Souls (2019)&quot;, &quot;src&quot;: &quot;https://upload.wikimedia.org/wikipedia/commons/2/2e/Amari%C3%A0_-_Funky_Souls_%282019%29.mp3&quot;, &quot;type&quot;: &quot;audio/mpeg&quot;}]' height='500px' width='500px' state='pause' track='0' start='0.0' end='' playbackrate='1.0' playbackmode='repeat' displaymode='controls-lg'></edirom-audio-player>";
